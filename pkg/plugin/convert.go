5cdda81c99c173ef09ae1ece71b6afe5016161b3
Ahmet Oeztuerk <Ahmet.Oeztuerk@consol.de>
2026-05-22 16:42:32 +0200

    add prelinimary support for array_of types , including string

--- pkg/plugin/convert.go
+++ pkg/plugin/convert.go
@@ -2,6 +2,7 @@ package plugin
 
 import (
 	"encoding/json"
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
@@ -63,27 +64,81 @@ func toBool(v interface{}) bool {
 	return false
 }
 
-func inferFieldType(name string, metaColumns map[string]columnMetadata) data.FieldType {
-	if mc, ok := metaColumns[name]; ok {
+func toString(v interface{}) string {
+	switch val := v.(type) {
+	case string:
+		return val
+	case int64:
+		return fmt.Sprintf("%d", val)
+	case float64:
+		return fmt.Sprintf("%f", val)
+	case bool:
+		return fmt.Sprintf("%t", val)
+	case []string:
+		return strings.Join(val, ",")
+	}
+	return ""
+}
+
+// To get all possible types use something like this:
+// [root@ebc5fefa4b7c V1]# pwd
+// /src/thruk/lib/Thruk/Controller/Rest/V1
+// [root@ebc5fefa4b7c V1]# grep -nrw '"type":' docs.pm > types.txt
+
+// parses the field types sent in Thruk function _get_columns_meta_for_path on API calls
+// https://github.com/sni/thruk/commit/8f56bb54633c48e33e1a6ed0ed6d5c5c8f2cc48f?diff=unified
+func inferFieldType(columnName string, metaColumns map[string]columnMetadata) (data.FieldType, string) {
+	if mc, ok := metaColumns[columnName]; ok {
+
 		if mc.GrafanaDataType != data.FieldTypeUnknown {
-			return mc.GrafanaDataType
+			return mc.GrafanaDataType, mc.Type
 		}
+
 		switch mc.Type {
 		case "number":
-			return data.FieldTypeFloat64
+			return data.FieldTypeFloat64, mc.Type
 		case "time":
-			return data.FieldTypeTime
+			return data.FieldTypeTime, mc.Type
 		case "bool", "boolean":
-			return data.FieldTypeBool
+			return data.FieldTypeBool, mc.Type
+		case "string":
+			return data.FieldTypeString, mc.Type
+		case "array_of_strings":
+			return data.FieldTypeString, mc.Type
+		default:
+			return data.FieldTypeUnknown, "unknown"
 		}
 	}
-	if strings.HasPrefix(name, "last_") || strings.HasPrefix(name, "next_") ||
-		strings.HasPrefix(name, "start_") || strings.HasPrefix(name, "end_") ||
-		strings.HasPrefix(name, "time") {
-		return data.FieldTypeTime
+
+	// there is no column metadata for that column
+	if strings.HasPrefix(columnName, "last_") || strings.HasPrefix(columnName, "next_") ||
+		strings.HasPrefix(columnName, "start_") || strings.HasPrefix(columnName, "end_") ||
+		strings.HasPrefix(columnName, "time") {
+		return data.FieldTypeTime, ""
 	}
-	if strings.HasPrefix(name, "time_") {
-		return data.FieldTypeFloat64
+
+	if strings.HasPrefix(columnName, "time_") {
+		return data.FieldTypeFloat64, ""
 	}
-	return data.FieldTypeString
+
+	return data.FieldTypeString, ""
+}
+
+// Parses the optional units added in Thruk function _get_columns_meta_for_path on API calls
+// https://github.com/sni/thruk/commit/8f56bb54633c48e33e1a6ed0ed6d5c5c8f2cc48f?diff=unified
+func modifyBasedOnUnitType(columnName string, metaColumns map[string]columnMetadata) {
+	if mc, ok := metaColumns[columnName]; ok {
+		if mc.Config != nil {
+			if cnfConverted, convOk := mc.Config.(struct{ Unit string }); convOk {
+				switch cnfConverted.Unit {
+				case "%":
+					// TODO: percentages are given as direct numbers? like 0.34 or 34
+					return
+				case "s":
+					//
+					return
+				}
+			}
+		}
+	}
 }
